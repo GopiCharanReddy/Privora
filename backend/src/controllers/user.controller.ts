8034280bc21c92d8be11536240c1b93ef55625d7
GopiCharanReddy <gopicharanvgcr@gmail.com>
2026-03-30 14:59:21 +0530

    update user route and controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -6,27 +6,35 @@ import { users } from "../db/schema.ts";
 export const userInfo = async (req: Request, res: Response) => {
   const { user } = req.body;
   if (!user) {
-    res.status(401).json({
+    return res.status(400).json({
       error: "Username cannot be null."
     });
   }
 
-  const existingUser = await db.query.users.findFirst({
-    where: (users, { eq }) => eq(users.id, 1),
-  });
-  if (existingUser) {
-    return res.status(409).json({
-      error: "User already exists."
+  try {
+    const existingUser = await db.query.users.findFirst({
+      where: (users, { eq }) => eq(users.id, 1),
+    });
+    
+    if (existingUser) {
+      return res.status(409).json({
+        error: "User already exists."
+      })
+    }
+  
+    const username = user + nanoid();
+  
+    await db.insert(users).values({
+      name: username,
+    }).returning({ insertedId: users.id });
+  
+    return res.status(200).json({
+      message: "User created successfully."
+    })
+  } catch (error) {
+    console.error("Error: ", error);
+    return res.status(500).json({
+      error: "Some error occurred. Please try again later"
     })
   }
-
-  const username = user + nanoid();
-
-  await db.insert(users).values({
-    name: username,
-  }).returning({ insertedId: users.id });
-
-  return res.status(200).json({
-    message: "User created successfully."
-  })
 }
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { userInfo } from '../controllers/user.controller.ts';
+import { createRoom, joinRoom } from '../controllers/room.controller.ts';
+
 const router = express.Router();
 
 router.post('/', userInfo);
