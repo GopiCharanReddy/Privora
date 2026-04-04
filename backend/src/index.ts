bc244d8c75b1e962a12b4692d39a194d3eb0e705
GopiCharanReddy <gopicharanvgcr@gmail.com>
2026-04-04 16:18:51 +0530

    add new frontend url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ import cors from "cors"
 dotenv.config()
 
 const corsOptions = {
-  origin: "https://privora.chaxan.in",
+  origin: ["https://privora.chaxan.in", "privora-eta.vercel.app"], 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
